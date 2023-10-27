7ecd89974fece3a6773c6ca683a20be26b33cd23
iolowookere217 <olowookere.isaaco@gmail.com>
2023-10-27 17:39:25 +0000

    Task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""A module that defines a class `LIFOCache`"""
+"""This module defines a class `LRUCache`"""
 
 from base_caching import BaseCaching
 from collections import deque
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """A caching system"""
 
     def __init__(self):
@@ -22,10 +22,15 @@ class LIFOCache(BaseCaching):
                 discarded_key = self.fifo_queue.pop()
                 del self.cache_data[discarded_key]
                 print("DISCARD: {}".format(discarded_key))
+            elif key in self.cache_data:
+                self.fifo_queue.remove(key)
 
             self.cache_data[key] = item
             self.fifo_queue.append(key)
 
     def get(self, key):
         """Gets an item by key."""
+        if key in self.cache_data:
+            self.fifo_queue.remove(key)
+            self.fifo_queue.append(key)
         return self.cache_data.get(key, None)
