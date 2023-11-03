7603722ef390f0183f8bfb40e7356e42ee8d2095
iolowookere217 <olowookere.isaaco@gmail.com>
2023-11-03 18:47:10 +0000

    Task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""This moduke sets up a basic Flask app"""
 
 from flask import Flask, render_template
 
@@ -7,7 +8,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """This function defines a route for the root URL ('/')"""
     return render_template("0-index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8080")
+    app.run(host="0.0.0.0", port="5080")
