2898a30d5a27126eda7ba644d9410c61b01a93c1
iolowookere217 <olowookere.isaaco@gmail.com>
2023-11-03 21:41:25 +0000

    Tasko

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -10,6 +10,3 @@ app = Flask(__name__)
 def index():
     """This function defines a route for the root URL ('/')"""
     return render_template("0-index.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5080")
