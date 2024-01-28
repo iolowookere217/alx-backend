80471d6120e22efa825bc88107e56ec2be4f1aea
iolowookere217 <olowookere.isaaco@gmail.com>
2024-01-28 08:20:44 +0000

    language translation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""This moduke sets up a basic Flask app"""
+'''Task 0: Basic Flask app
+'''
 
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    """This function defines a route for the root URL ('/')"""
-    return render_template("0-index.html")
+    '''default route'''
+    return render_template("0-index.html",)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
